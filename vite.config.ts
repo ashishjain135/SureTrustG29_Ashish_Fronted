2c3d315e785aa17eff3a6a8124372d9e9b49e0bd
Ashish Jain <ashishfarnish135@gmail.com>
2026-01-16 21:18:53 +0530

    update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+
+//   preview: {
+//     allowedHosts: ["https://suretrustg29-ashish-fronted.onrender.com"],
+//     host: "0.0.0.0",
+//     port: Number(process.env.PORT) || 4173,
+//   },
+// });
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
   preview: {
-    allowedHosts: ["https://suretrustg29-ashish-fronted.onrender.com"],
+    allowedHosts: ["suretrustg29-ashish-fronted.onrender.com"],
     host: "0.0.0.0",
-    port: Number(process.env.PORT) || 4173,
+    port: 4173,
   },
 });
