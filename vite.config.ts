6e91a299c74e3f2091283ab838e795050a696671
Ashish Jain <ashishfarnish135@gmail.com>
2026-01-16 20:59:27 +0530

    vite confi

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
 
   preview: {
-    allowedHosts: ["social-media-frontend-st-g29.onrender.com"],
+    allowedHosts: ["https://suretrustg29-ashish-fronted.onrender.com"],
     host: "0.0.0.0",
     port: Number(process.env.PORT) || 4173,
   },
