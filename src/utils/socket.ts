55c60133a272748b1e6653c7477095ca7327f167
Ashish Jain <ashishfarnish135@gmail.com>
2026-01-17 14:33:23 +0530

    update chatWindow

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -3,7 +3,7 @@ export const socket = io(
   "https://suretrustg29fsd-ashish.onrender.com",
   {
     path: "/socket.io",
-    transports: ["polling", "websocket"],
+    transports: ["polling"],
     autoConnect: true,
   }
 );
